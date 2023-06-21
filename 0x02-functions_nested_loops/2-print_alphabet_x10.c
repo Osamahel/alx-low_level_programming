d9b9b9182a74946a9def381e3f09d8a31a7ec66d
osama <osama143helmy@gmail.com>
2023-06-21 16:47:33 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function that will print the alphabet 10 times
+ * print_alphabet_x10 - function print a to z 0 times
  *
  * Return: void
 */
+
 void print_alphabet_x10(void)
 {
-	int line, ch;
+	int i;
 
-	for (line = 0, line <= 9, line++)
+	for (i = 0; i < 10; i++)
 	{
-		for (ch 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 		_putchar('\n');
 	}
 }
