0ec46d1138a8d499c1d3012418bce2330c7681d1
osama <osama143helmy@gmail.com>
2023-06-21 19:10:31 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Sums two integers and return the resault
+ *
+ * @a: integer one
+ * @b: integer two
+ *
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+
+	int sum = a + b;
+
+	return (sum);
+}
