1321875e37f4a21daafd6c39ab0d31c3a36a4938
osama <osama143helmy@gmail.com>
2023-06-22 20:00:20 -0700

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: the integer to print
+ *
+ * Return: void
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		num = n;
+		_putchar('-');
+	}
+
+	num /= 10;
+
+	if (num != 0)
+		print_number(num);
+
+	_putchar((unsigned int) n % 10 + '0');
+}
